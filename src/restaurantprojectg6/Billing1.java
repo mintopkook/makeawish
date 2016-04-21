996087c37e1301994536d22d2e72d9bda56971d6
kunothiiz <kunothiiz@kunothiiz-PC>
2016-04-22 01:27:05 +0700

    Testing by add more function

--- src/restaurantprojectg6/Billing1.java
+++ src/restaurantprojectg6/Billing1.java
@@ -73,7 +73,9 @@ public class Billing1 extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        dispose();
+        BillTable bt = new BillTable();
+        bt.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
