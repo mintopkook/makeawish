bfba1ed65730a74b844618a7a7b8ab4abfed1701
mintopkook <mintopkook@hotmail.com>
2016-05-03 03:25:18 +0700

    Billing1 fin

--- src/restaurantprojectg6/Billing1.java
+++ src/restaurantprojectg6/Billing1.java
@@ -30,10 +30,10 @@ public class Billing1 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         DoneButtom1 = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -42,39 +42,41 @@ public class Billing1 extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Bangna New", 1, 36)); // NOI18N
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Billing_logo.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 750, 190));
+        jLabel4.setFont(new java.awt.Font("#TS  Malee Normal", 0, 55)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Billing");
+        jLabel4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 50, 210, 60));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/head1.png"))); // NOI18N
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 30, 580, 90));
 
         jButton1.setBackground(new java.awt.Color(204, 150, 138));
         jButton1.setFont(new java.awt.Font("Bangna New", 1, 18)); // NOI18N
         jButton1.setForeground(new java.awt.Color(240, 240, 240));
-        jButton1.setText("Select Table ");
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/select.png"))); // NOI18N
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 260, 189, 74));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 250, 320, 100));
 
         DoneButtom1.setBackground(new java.awt.Color(255, 255, 255));
-        DoneButtom1.setFont(new java.awt.Font("Adobe Caslon Pro", 1, 18)); // NOI18N
+        DoneButtom1.setFont(new java.awt.Font("#TS  Malee Normal", 1, 18)); // NOI18N
         DoneButtom1.setText("BACK");
         DoneButtom1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 DoneButtom1ActionPerformed(evt);
             }
         });
-        getContentPane().add(DoneButtom1, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 370, 90, 40));
+        getContentPane().add(DoneButtom1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 470, 90, 40));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/icon-billing.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 200, 230, 230));
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/bg.png"))); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/bg2.png"))); // NOI18N
         jLabel2.setMaximumSize(new java.awt.Dimension(896, 600));
         jLabel2.setMinimumSize(new java.awt.Dimension(896, 600));
         jLabel2.setPreferredSize(new java.awt.Dimension(896, 600));
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 600));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -132,8 +134,8 @@ public class Billing1 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton DoneButtom1;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     // End of variables declaration//GEN-END:variables
 }
