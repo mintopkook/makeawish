806230c992585dcf23c52143dfd7f7ed4a9cae5c
mintopkook <mintopkook@hotmail.com>
2016-05-03 03:22:56 +0700

    order fin

--- src/restaurantprojectg6/Order2.java
+++ src/restaurantprojectg6/Order2.java
@@ -40,85 +40,31 @@ public class Order2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        coke = new javax.swing.JButton();
-        pepsi = new javax.swing.JButton();
-        cake = new javax.swing.JButton();
-        cupcake = new javax.swing.JButton();
-        spaghetti = new javax.swing.JButton();
-        ramen = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         clear = new javax.swing.JButton();
         priceLabel = new javax.swing.JLabel();
         back = new javax.swing.JButton();
         doneButton = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         tableLabel = new javax.swing.JLabel();
+        total = new javax.swing.JLabel();
+        notable = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        cake = new javax.swing.JButton();
+        ramen = new javax.swing.JButton();
+        spaghetti = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        pepsi = new javax.swing.JButton();
+        coke = new javax.swing.JButton();
+        cupcake = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        coke.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
-        coke.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/water.png"))); // NOI18N
-        coke.setText("Coke");
-        coke.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cokeActionPerformed(evt);
-            }
-        });
-        getContentPane().add(coke, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 490, 150, 70));
-
-        pepsi.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
-        pepsi.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/water.png"))); // NOI18N
-        pepsi.setText("Pepsi");
-        pepsi.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pepsiActionPerformed(evt);
-            }
-        });
-        getContentPane().add(pepsi, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 410, 150, -1));
-
-        cake.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
-        cake.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/cake_A-512.png"))); // NOI18N
-        cake.setText("cake");
-        cake.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cakeActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cake, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 410, 160, -1));
-
-        cupcake.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
-        cupcake.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/cupp.png"))); // NOI18N
-        cupcake.setText("cupcake");
-        cupcake.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cupcakeActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cupcake, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 490, 160, 70));
-
-        spaghetti.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/spa.png"))); // NOI18N
-        spaghetti.setText(" Spaghetti");
-        spaghetti.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                spaghettiActionPerformed(evt);
-            }
-        });
-        getContentPane().add(spaghetti, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 490, 140, 70));
-
-        ramen.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
-        ramen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/noodle.png"))); // NOI18N
-        ramen.setText("Ramen");
-        ramen.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ramenActionPerformed(evt);
-            }
-        });
-        getContentPane().add(ramen, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 410, 140, -1));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -128,9 +74,10 @@ public class Order2 extends javax.swing.JFrame {
                 "Menu", "Price"
             }
         ));
+        jTable1.setSelectionForeground(new java.awt.Color(204, 255, 204));
         jScrollPane1.setViewportView(jTable1);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 140, 400, 220));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 130, 440, 220));
 
         clear.setText("Clear");
         clear.addActionListener(new java.awt.event.ActionListener() {
@@ -138,15 +85,20 @@ public class Order2 extends javax.swing.JFrame {
                 clearActionPerformed(evt);
             }
         });
-        getContentPane().add(clear, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 490, -1, -1));
+        getContentPane().add(clear, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 450, -1, -1));
 
-        priceLabel.setFont(new java.awt.Font("Lucida Sans Unicode", 0, 18)); // NOI18N
+        priceLabel.setFont(new java.awt.Font("Perpetua", 0, 24)); // NOI18N
         priceLabel.setForeground(new java.awt.Color(255, 255, 204));
         priceLabel.setText("Total price: ");
-        getContentPane().add(priceLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 360, 153, -1));
+        getContentPane().add(priceLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 370, 120, -1));
 
         back.setText("Back");
-        getContentPane().add(back, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 560, -1, -1));
+        back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backActionPerformed(evt);
+            }
+        });
+        getContentPane().add(back, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 520, -1, -1));
 
         doneButton.setText("Done");
         doneButton.addActionListener(new java.awt.event.ActionListener() {
@@ -154,24 +106,115 @@ public class Order2 extends javax.swing.JFrame {
                 doneButtonActionPerformed(evt);
             }
         });
-        getContentPane().add(doneButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 530, -1, -1));
+        getContentPane().add(doneButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 520, -1, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/002.png"))); // NOI18N
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 100, 190, 300));
 
         jLabel2.setFont(new java.awt.Font("#TS  Malee Normal", 0, 55)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("ORDER");
         jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 30, 210, 60));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 40, 210, 60));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/head1.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, 560, 80));
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/head1.png"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 20, 580, 90));
 
-        tableLabel.setFont(new java.awt.Font("Lucida Fax", 0, 18)); // NOI18N
+        tableLabel.setFont(new java.awt.Font("Perpetua", 0, 24)); // NOI18N
         tableLabel.setForeground(new java.awt.Color(255, 255, 204));
         tableLabel.setText("Table: ");
-        getContentPane().add(tableLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, -1, -1));
+        getContentPane().add(tableLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 70, 60));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/bg2.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -10, 940, 620));
+        total.setForeground(new java.awt.Color(255, 255, 0));
+        total.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 153, 153)));
+        getContentPane().add(total, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 370, 90, 30));
+
+        notable.setForeground(new java.awt.Color(255, 255, 204));
+        notable.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 153, 153)));
+        getContentPane().add(notable, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 150, 60, 40));
+
+        jPanel2.setLayout(new java.awt.GridLayout(1, 0));
+
+        cake.setBackground(new java.awt.Color(102, 102, 0));
+        cake.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
+        cake.setForeground(new java.awt.Color(102, 102, 0));
+        cake.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/cake_A-512.png"))); // NOI18N
+        cake.setText("cake");
+        cake.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cakeActionPerformed(evt);
+            }
+        });
+        jPanel2.add(cake);
+
+        ramen.setBackground(new java.awt.Color(102, 102, 0));
+        ramen.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
+        ramen.setForeground(new java.awt.Color(102, 102, 0));
+        ramen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/noodle.png"))); // NOI18N
+        ramen.setText("Ramen");
+        ramen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ramenActionPerformed(evt);
+            }
+        });
+        jPanel2.add(ramen);
+
+        spaghetti.setBackground(new java.awt.Color(102, 102, 0));
+        spaghetti.setForeground(new java.awt.Color(102, 102, 0));
+        spaghetti.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/spa.png"))); // NOI18N
+        spaghetti.setText(" Spaghetti");
+        spaghetti.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                spaghettiActionPerformed(evt);
+            }
+        });
+        jPanel2.add(spaghetti);
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 490, 530, 70));
+
+        jPanel1.setLayout(new java.awt.GridLayout(1, 0));
+
+        pepsi.setBackground(new java.awt.Color(102, 102, 0));
+        pepsi.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
+        pepsi.setForeground(new java.awt.Color(102, 102, 0));
+        pepsi.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/water.png"))); // NOI18N
+        pepsi.setText("Pepsi");
+        pepsi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pepsiActionPerformed(evt);
+            }
+        });
+        jPanel1.add(pepsi);
+
+        coke.setBackground(new java.awt.Color(102, 102, 0));
+        coke.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
+        coke.setForeground(new java.awt.Color(102, 102, 0));
+        coke.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/water.png"))); // NOI18N
+        coke.setText("Coke");
+        coke.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cokeActionPerformed(evt);
+            }
+        });
+        jPanel1.add(coke);
+
+        cupcake.setBackground(new java.awt.Color(102, 102, 0));
+        cupcake.setFont(new java.awt.Font("Lucida Grande", 0, 16)); // NOI18N
+        cupcake.setForeground(new java.awt.Color(102, 102, 0));
+        cupcake.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/cupp.png"))); // NOI18N
+        cupcake.setText("cupcake");
+        cupcake.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cupcakeActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cupcake);
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 410, 530, 70));
+
+        jLabel1.setForeground(new java.awt.Color(102, 102, 0));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/bg2.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -319,6 +362,10 @@ public class Order2 extends javax.swing.JFrame {
         new Function1().setVisible(true);
     }//GEN-LAST:event_doneButtonActionPerformed
 
+    private void backActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_backActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -364,12 +411,17 @@ public class Order2 extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JLabel notable;
     private javax.swing.JButton pepsi;
     private javax.swing.JLabel priceLabel;
     private javax.swing.JButton ramen;
     private javax.swing.JButton spaghetti;
     private javax.swing.JLabel tableLabel;
+    private javax.swing.JLabel total;
     // End of variables declaration//GEN-END:variables
 }
