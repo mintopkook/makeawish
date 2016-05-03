3253c0477167987cb7c8bc04576b660d3959755d
mintopkook <mintopkook@hotmail.com>
2016-05-04 00:28:47 +0700

    Backlog view

--- src/view/BackLog_view.java
+++ src/view/BackLog_view.java
@@ -5,6 +5,8 @@
  */
 package view;
 
+import restaurantprojectg6.Function1;
+
 /**
  *
  * @author minto
@@ -15,8 +17,8 @@ public class BackLog_view extends javax.swing.JFrame {
      * Creates new form BackLog_view
      */
     public BackLog_view() {
-       initComponents();
-        setSize(800,600);
+        initComponent();
+        setSize(800, 600);
     }
 
     /**
@@ -43,10 +45,96 @@ public class BackLog_view extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+   private void initComponent() {
+
+        popFood = new javax.swing.JLabel();
+        noCustomer = new javax.swing.JLabel();
+        NumCustxt2 = new javax.swing.JTextField();
+        DoneButtom2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        NumCusTxt1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 204, 204));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        popFood.setFont(new java.awt.Font("Perpetua", 0, 36)); // NOI18N
+        popFood.setForeground(new java.awt.Color(224, 224, 55));
+        popFood.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/golden_star.png"))); // NOI18N
+        popFood.setText("Popular Food");
+        popFood.setToolTipText("");
+        getContentPane().add(popFood, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 200, 290, 70));
+
+        noCustomer.setFont(new java.awt.Font("Perpetua", 0, 36)); // NOI18N
+        noCustomer.setForeground(new java.awt.Color(224, 224, 55));
+        noCustomer.setText("Number of Customer");
+        getContentPane().add(noCustomer, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 310, 290, 80));
+
+        NumCustxt2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NumCustxt2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(NumCustxt2, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 220, 300, 40));
+
+        DoneButtom2.setBackground(new java.awt.Color(255, 255, 255));
+        DoneButtom2.setFont(new java.awt.Font("Perpetua", 1, 18)); // NOI18N
+        DoneButtom2.setText("BACK");
+        DoneButtom2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DoneButtom2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(DoneButtom2, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 480, 90, 40));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/cakey.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 130, 170, 390));
+
+        NumCusTxt1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NumCusTxt1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(NumCusTxt1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 330, 200, 40));
+
+        jLabel4.setFont(new java.awt.Font("#TS  Malee Normal", 0, 55)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Backlog");
+        jLabel4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 50, 200, 80));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/head1.png"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 50, 650, 80));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/restaurantprojectg6/Picture/bg2.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 600));
+
+        pack();
+    }// </editor-fold>        
 
     /**
      * @param args the command line arguments
      */
+   
+   
+    private void DoneButtom2ActionPerformed(java.awt.event.ActionEvent evt) {
+        dispose();
+        Function1 f = new Function1();
+        f.setVisible(true);
+    }
+
+    private void NumCustxt2ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void NumCusTxt1ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -79,6 +167,19 @@ public class BackLog_view extends javax.swing.JFrame {
         });
     }
 
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
+
+    
+    private javax.swing.JButton DoneButtom2;
+    private javax.swing.JTextField NumCusTxt1;
+    private javax.swing.JTextField NumCustxt2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel noCustomer;
+    private javax.swing.JLabel popFood;
+
 }
